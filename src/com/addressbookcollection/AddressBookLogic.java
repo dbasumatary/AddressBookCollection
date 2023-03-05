119036a9e0a93eacdc3d38967a38a04e70ef85c2
dbasumatary <basudhondip@gmail.com>
2023-03-05 14:42:57 +0530

    Added: Person and Address Details

---
+++ src/com/addressbookcollection/AddressBookLogic.java
@@ -0,0 +1,7 @@
+package com.addressbookcollection;
+import java.util.ArrayList;
+
+/*Writing all the logic*/
+public class AddressBookLogic {
+    ArrayList<Contacts> contact = new ArrayList<Contacts>();
+}
